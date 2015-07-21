2b61d5efb2c4d112d2c1359e8145515515e3ba46
Mamadou Kaba <mamkab23@gmail.com>
2015-07-21 10:33:08 -0400

    Set Up PreferenceViewController.swift

--- Chillapp/PreferenceViewController.swift
+++ Chillapp/PreferenceViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import QuadratTouch
 
 
 class PreferenceViewController: UIViewController {
     @IBOutlet weak var distanceLabel: UILabel!
     
+    @IBOutlet weak var searchBar: UISearchBar!
    
     @IBAction func labelChanged(sender: UISlider) {
         var sliderValue = lroundf(sender.value)
@@ -21,18 +23,24 @@ class PreferenceViewController: UIViewController {
     var sliderValue = 0
     var searchbar = 0
     
+    
     @IBAction func searchButton(sender: UIButton) {
+        let client = Client(clientID:       "QOYLSJXAXYV5GEHMZAUSFUGGQX0QBWFSLRPN25PHGKBDT0LJ",
+            clientSecret:   "TTCDU05B0DKBGTXI1AZSGHWQDGDLMTYFNHTHH32AHLDIHPGE",
+            redirectURL:    "testapp123://foursquare")
+        var configuration = Configuration(client:client)
+        Session.setupSharedSessionWithConfiguration(configuration)
+        
+        let session = Session.sharedSession()
         var parameters = [Parameter.query:"Burgers"]
-        parameters += self.location.parameters()
+        //parameters += self.location.parameters()
         let searchTask = session.venues.search(parameters) {
             (result) -> Void in
             if let response = result.response {
-                self.venues = response["venues"] as [JSONParameters]?
-                self.tableView.reloadData()
+                //self.venues = response["venues"] as [JSONParameters]?
+                //self.tableView.reloadData()
             }
         }
         searchTask.start()
     }
-    
-    
 }
